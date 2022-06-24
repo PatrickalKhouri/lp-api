2d24a4740ee125ef931f1549571a1593222f5650
Patrick Khouri <patrick.darya@gmail.com>
2022-06-24 19:04:32 -0300

    Installed TypeOrm

---
+++ src/api/test.js
@@ -0,0 +1,16 @@
+const pgtools = require('pgtools');
+
+const config = {
+  user: 'postgres',
+  host: 'localhost',
+  password: 'postgres',
+  port: 5432,
+};
+
+pgtools.createdb(config, 'some_db', (err, res) => {
+  if (err) {
+    console.error(err);
+    process.exit(-1);
+  }
+  console.log(res);
+});
